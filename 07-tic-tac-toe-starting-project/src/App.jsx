9b2e8d42b6aeaedc545116349614464c356bb113
karim <karimalaa298@gmail.com>
2025-11-23 10:33:27 +0300

    add basic ui for tic tac toe

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,29 @@
 
-function App() {
-  
 
+function App() {
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>              
+            </span>
+          <button>edit</button>            
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>              
+            </span>
+          <button>edit</button>            
+          </li>          
+        </ol>
+        GAME BOARD
+      </div>
+      LOGO
+    </main>
   )
 }
 
